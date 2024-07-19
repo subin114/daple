33f20b4e98fc0a14c1cc620e3fc72b59ac7f7ee9
subin114 <bin7851@naver.com>
2024-07-19 14:00:09 +0900

    🔧 초기 설정 및 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./router/router";
+import Nav from "./components/layout/Nav";
+import Footer from "./components/layout/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <ChakraProvider>
+        <BrowserRouter>
+          <Nav />
+          <Router />
+          <Footer />
+        </BrowserRouter>
+      </ChakraProvider>
+    </>
+  );
 }
 
 export default App;
