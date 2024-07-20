4193f7acf2849db72cabaebaaff3bc27a9ed98f9
subin114 <bin7851@naver.com>
2024-07-20 12:25:58 +0900

    🔧 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
-import Router from "./router/router";
-import Nav from "./components/layout/Nav";
-import Footer from "./components/layout/Footer";
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './router/router';
+import Nav from './components/layout/Nav';
+import Footer from './components/layout/Footer';
+import styled from '@emotion/styled';
 
 function App() {
   return (
     <>
       <ChakraProvider>
         <BrowserRouter>
-          <Nav />
+          <NavFixed>
+            <Nav />
+          </NavFixed>
           <Router />
           <Footer />
         </BrowserRouter>
@@ -17,5 +20,10 @@ function App() {
     </>
   );
 }
+
+const NavFixed = styled.div`
+  position: relative;
+  height: 70px;
+`;
 
 export default App;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,27 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "../components/pages/Home";
-import { memo } from "react";
+import { Routes, Route } from 'react-router-dom';
+import Home from '../components/pages/Home';
+import { memo } from 'react';
+import Near from '../components/pages/Near';
+import Region from '../components/pages/Region';
+import Community from '../components/pages/Community';
+import BookMark from '../components/pages/BookMark';
+import MyPage from '../components/pages/MyPage';
+import Login from '../components/pages/Login';
+import SignUp from '../components/pages/SignUp';
+import PopularList from '../components/layout/PopularList';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/near" element={<Near />} />
+      <Route path="/popular" element={<PopularList />} />
+      <Route path="/region" element={<Region />} />
+      <Route path="/community" element={<Community />} />
+      <Route path="/bookmark" element={<BookMark />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 };
