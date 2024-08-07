18d90277fcdb77d028b8da0e9c770b7bebd86fbb
subin114 <bin7851@naver.com>
2024-08-07 23:22:58 +0900

    ✨ 서브 컬렉션 comments 생성

--- src/firebase/firestoreConfig.ts
+++ src/firebase/firestoreConfig.ts
@@ -55,3 +55,26 @@ export const savePost = async (content: string, uid: string, nickname: string) =
     console.error('Error:', err);
   }
 };
+
+/** comment 컬렉션 - 댓글 저장 */
+export const saveComment = async (id: string, content: string, uid: string, nickname: string) => {
+  try {
+    const docRef = await addDoc(collection(db, 'posts', id, 'comments'), {
+      content,
+      uid,
+      nickname,
+      createdAt: new Date(),
+    });
+
+    console.log('댓글이 성공적으로 저장되었습니다.');
+    return {
+      id: docRef.id,
+      content,
+      uid,
+      nickname,
+      createdAt: new Date(),
+    };
+  } catch (err) {
+    console.error('Error saving comment:', err);
+  }
+};
