17f4afa779c739ff7eda945c99235ef495a49771
subin114 <bin7851@naver.com>
2024-08-14 20:26:30 +0900

    ✨ ScrollToTop 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Router from './router/Router';
 import Nav from './components/layout/Nav';
 import styled from '@emotion/styled';
 import AuthInitializer from './firebase/AuthInitializer';
+import ScrollToTop from './utils/ScrollToTop';
 
 function App() {
   return (
     <>
       <ChakraProvider>
         <BrowserRouter>
+          <ScrollToTop />
           <AuthInitializer />
           <NavFixed>
             <Nav />
